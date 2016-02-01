2b8cfe15be754acd9e441816a61ee1ef79387235
Manon4ik <manon4ik@gmail.com>
2016-02-01 12:01:55 +0200

    Научили управлять змейкой с помощью клавишь.

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -27,18 +27,19 @@ namespace snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
-            snake.Move();
+            
+            while(true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+                System.Threading.Thread.Sleep(150);
+                snake.Move();
+            }
 
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-
-            Console.ReadLine();
+            //Console.ReadLine();
         }
 
     }
