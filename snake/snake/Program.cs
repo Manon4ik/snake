9cfb41b10a401e523a42fc8d3b6824d8136a1aea
Manon4ik <manon4ik@gmail.com>
2016-02-01 13:58:23 +0200

    Змейка кушает и растет!

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -27,16 +27,32 @@ namespace snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
             
             while(true)
             {
+                if (snake.Eat(food) )
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                System.Threading.Thread.Sleep(350);
+
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                System.Threading.Thread.Sleep(150);
-                snake.Move();
+                
+                
             }
 
             //Console.ReadLine();
